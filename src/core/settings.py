b94cc5cb1588e0c6f4fed7e82422393238ab58d2
alex_rokhman <alex.bobr@rambler.ru>
2019-12-04 00:15:55 +0300

    Added dynaconf

--- src/core/settings.py
+++ src/core/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+from dynaconf import settings as _settings
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,12 +22,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'osx!*ihr0uk83!^$hh!bdo(m^w1vkc2y0o%3*nmo%6j*9b5l!3'
+SECRET_KEY = _settings.SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = _settings.DEBUG
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", "schedulebntu.herokuapp.com"]
+ALLOWED_HOSTS = _settings.ALLOWED_HOSTS
 
 
 # Application definition
